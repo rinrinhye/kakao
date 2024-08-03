3a49fd5479a01a91b60de12c6d590c222bc184f9
ririririririn <123622271+ririririririn@users.noreply.github.com>
2024-08-03 09:42:34 +0900

    share-overlay 정리

--- js/card-overlay.js
+++ js/card-overlay.js
@@ -1,39 +1,32 @@
-const overlays = document.querySelectorAll(
-  ".news-card-slide-wrap .share-overlay-group"
-);
+const overlays = document.querySelectorAll('.share-overlay');
 
-const shareButtons = document.querySelectorAll(
-  ".news-card-slide-wrap .share-button"
-);
+const shareButtons = document.querySelectorAll('.info .share-button');
 
 const slideArrowButtons = document.querySelectorAll(
-  ".news-card-slide-wrap .news-card-slide-arrows"
+  '.news-card-slide-wrap .news-card-slide-arrows'
 );
 
 const closeButtons = document.documentElement.querySelectorAll(
-  ".share-overlay-group .close-button"
+  '.share-overlay .close-button'
 );
 
-// shareButtons.addEventListener("click", (e) => {
-//   e.target.classList.toggle("is-disabled");
-//   console.log(e.target);
-//   overlay.classList.toggle("is-active");
-//   // slideArrowButtons.classList.toggle("is-disabled");
-// });
-
 shareButtons.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    button.classList.add("is-disabled");
-    let overlay = e.target.closest(".share-button").nextElementSibling;
-    overlay.classList.add("is-active");
+  button.addEventListener('click', (e) => {
+    button.classList.add('is-disabled');
+
+    let overlay = button.parentElement.parentElement
+      .closest('.card')
+      .querySelector('.share-overlay');
+
+    overlay.classList.add('is-active');
   });
 });
 
 closeButtons.forEach((closeButton) => {
-  closeButton.addEventListener("click", (e) => {
-    let overlay = e.target.closest(".share-overlay-group");
-    let shareButton = overlay.previousElementSibling;
-    shareButton.classList.remove("is-disabled");
-    overlay.classList.remove("is-active");
+  closeButton.addEventListener('click', (e) => {
+    let overlay = e.target.closest('.share-overlay');
+    let shareButton = overlay.parentElement.querySelector('.share-button');
+    shareButton.classList.remove('is-disabled');
+    overlay.classList.remove('is-active');
   });
 });
