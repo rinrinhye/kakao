5198bd4e49bbbe09c4199d3b3429944ab54b9eee
ririririririn <123622271+ririririririn@users.noreply.github.com>
2024-08-03 14:55:37 +0900

    story-slide rolling 애니메이션 추가

--- js/story-slide.js
+++ js/story-slide.js
@@ -1,9 +1,47 @@
-$(".story-slide-list").marquee({
-  duration: 50000,
-  direction: "left",
-  delayBeforeStart: 0,
-  startVisible: true,
-  duplicated: true,
-  pauseOnHover: true,
-  gap: 0,
+const sectionStory = document.querySelector('.section-story');
+const storySlideWrapper = sectionStory.querySelector('.story-slide-wrapper');
+const button = sectionStory.querySelector('.slide-button');
+const storySlideList = document.querySelector('.story-slide-list');
+const cloneStorySlideList = storySlideList.cloneNode(true);
+storySlideList.after(cloneStorySlideList);
+
+const storySlideLists = document.querySelectorAll('.story-slide-list');
+
+let click = false;
+
+button.addEventListener('click', () => {
+  if (button.classList.contains('is-paused')) {
+    play();
+  } else {
+    puased();
+  }
+  click = !click;
+});
+
+function puased() {
+  button.classList.add('is-paused');
+  storySlideLists.forEach((list) => {
+    list.style.animationPlayState = 'paused';
+  });
+}
+
+function play() {
+  button.classList.remove('is-paused');
+  storySlideLists.forEach((list) => {
+    list.style.animationPlayState = 'running';
+  });
+}
+
+storySlideWrapper.addEventListener('mouseenter', () => {
+  if (click) return;
+  else {
+    puased();
+  }
+});
+
+storySlideWrapper.addEventListener('mouseleave', () => {
+  if (click) return;
+  else {
+    play();
+  }
 });
