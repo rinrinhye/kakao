88f9bc4fdd4ab3ab1dbf8b9ed476d707f3288dfd
ririririririn <123622271+ririririririn@users.noreply.github.com>
2024-08-03 15:36:15 +0900

    news-card-slide arrow 버튼 토글 이벤트 추가

--- js/card-overlay.js
+++ js/card-overlay.js
@@ -2,7 +2,7 @@ const overlays = document.querySelectorAll('.share-overlay');
 
 const shareButtons = document.querySelectorAll('.info .share-button');
 
-const slideArrowButtons = document.querySelectorAll(
+const slideArrowButtonGroup = document.querySelector(
   '.news-card-slide-wrap .news-card-slide-arrows'
 );
 
@@ -14,19 +14,41 @@ shareButtons.forEach((button) => {
   button.addEventListener('click', (e) => {
     button.classList.add('is-disabled');
 
-    let overlay = button.parentElement.parentElement
-      .closest('.card')
-      .querySelector('.share-overlay');
+    const card = button.parentElement.parentElement.closest('.card');
 
+    const overlay = card.querySelector('.share-overlay');
     overlay.classList.add('is-active');
+
+    if (window.innerWidth < 1024) {
+      if (card.classList.contains('news-card')) {
+        slideArrowButtonGroup.classList.add('is-disabled');
+      }
+    }
   });
 });
 
 closeButtons.forEach((closeButton) => {
   closeButton.addEventListener('click', (e) => {
-    let overlay = e.target.closest('.share-overlay');
-    let shareButton = overlay.parentElement.querySelector('.share-button');
+    const card = e.target.closest('.card');
+    console.log(card);
+    const overlay = e.target.closest('.share-overlay');
+    const shareButton = overlay.parentElement.querySelector('.share-button');
     shareButton.classList.remove('is-disabled');
     overlay.classList.remove('is-active');
+
+    if (window.innerWidth < 1024) {
+      if (card.classList.contains('news-card')) {
+        slideArrowButtonGroup.classList.remove('is-disabled');
+      }
+    }
   });
+});
+
+window.addEventListener('resize', () => {
+  if (
+    window.innerWidth >= 1024 &&
+    slideArrowButtonGroup.classList.contains('is-disabled')
+  ) {
+    slideArrowButtonGroup.classList.remove('is-disabled');
+  }
 });
