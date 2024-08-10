3cbe5a02fedcbea742e2208df1b2fab16d28f1b6
rinrinhye <115080424+rinrinhye@users.noreply.github.com>
2024-08-10 20:03:21 +0900

    gnb 웹 접근성 개선

--- js/gnb.js
+++ js/gnb.js
@@ -1,51 +1,64 @@
-const gnbItems = document.documentElement.querySelectorAll('.gnb-item');
+const gnbItems = document.documentElement.querySelectorAll('.header .gnb-item');
 
 let currentGnbItem;
 
 gnbItems.forEach((item) => {
-  item.addEventListener('click', showAndHideLnb);
+	item.addEventListener('click', showAndHideLnb);
 });
 
 function showAndHideLnb(e) {
-  if (e.target.closest('.gnb-item').classList.contains('link')) return;
+	e.stopPropagation();
+	if (e.target.closest('.gnb-item').classList.contains('link')) return;
 
-  if (!currentGnbItem) {
-    /* 선택된 gnb 없을 때 */
-    currentGnbItem = e.target;
-    addClassListforCurrentGnbItem();
+	if (!currentGnbItem) {
+		/* 선택된 gnb 없을 때 */
+		currentGnbItem = e.target.closest('.gnb-item');
+		changeAriaExpandedtoTrue();
+		addClassListforCurrentGnbItem();
 
-    /* 윈도우 클릭 이벤트 추가 */
-    window.addEventListener('click', closeGnbItemonClilkingOutside);
-  } else {
-    /* 선택된 gnb 있을 때 */
-    if (currentGnbItem === e.target) {
-      /* current 있고 타겟 같음 */
-      removeClassListforCurrentGnbItem();
-      currentGnbItem = null;
+		/* 윈도우 클릭 이벤트 추가 */
+		window.addEventListener('click', closeGnbItemonClilkingOutside);
+	} else {
+		/* 선택된 gnb 있을 때 */
+		if (currentGnbItem === e.target.closest('.gnb-item')) {
+			/* current 있고 타겟 같음 */
+			changeAriaExpandedtoFalse();
+			removeClassListforCurrentGnbItem();
+			currentGnbItem = null;
 
-      window.removeEventListener('click', closeGnbItemonClilkingOutside);
-    } else {
-      /* current 있고 타겟 다름 */
-      removeClassListforCurrentGnbItem();
-      currentGnbItem = e.target;
-      addClassListforCurrentGnbItem();
-    }
-  }
+			window.removeEventListener('click', closeGnbItemonClilkingOutside);
+		} else {
+			/* current 있고 타겟 다름 */
+			changeAriaExpandedtoFalse();
+			removeClassListforCurrentGnbItem();
+			currentGnbItem = e.target.closest('.gnb-item');
+			changeAriaExpandedtoTrue();
+			addClassListforCurrentGnbItem();
+		}
+	}
 }
 
 function removeClassListforCurrentGnbItem() {
-  currentGnbItem.classList.remove('is-active');
+	currentGnbItem.classList.remove('is-active');
 }
 
 function addClassListforCurrentGnbItem() {
-  currentGnbItem.classList.add('is-active');
+	currentGnbItem.classList.add('is-active');
+}
+
+function changeAriaExpandedtoTrue() {
+	currentGnbItem.firstElementChild.setAttribute('aria-expanded', 'true');
+}
+function changeAriaExpandedtoFalse() {
+	currentGnbItem.firstElementChild.setAttribute('aria-expanded', 'false');
 }
 
 function closeGnbItemonClilkingOutside(e) {
-  if (!e.target.contains(currentGnbItem)) {
-    removeClassListforCurrentGnbItem();
-    currentGnbItem = null;
+	if (!e.target.contains(currentGnbItem)) {
+		currentGnbItem.firstElementChild.setAttribute('aria-expanded', 'false');
+		removeClassListforCurrentGnbItem();
+		currentGnbItem = null;
 
-    window.removeEventListener('click', closeGnbItemonClilkingOutside);
-  }
+		window.removeEventListener('click', closeGnbItemonClilkingOutside);
+	}
 }
