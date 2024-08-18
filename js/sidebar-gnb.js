f70e7361679b0d31e18b43a2111949970102ddf9
rinrinhye <115080424+rinrinhye@users.noreply.github.com>
2024-08-18 20:49:14 +0900

    sidebar-gnb 접근성 개선

--- js/sidebar-gnb.js
+++ js/sidebar-gnb.js
@@ -3,25 +3,44 @@ const sidebarGnb = document.querySelector('.sidebar-gnb');
 const closeButton = sidebarGnb.querySelector('.close-button');
 const sidebarGnbItems = sidebarGnb.querySelectorAll('.gnb-item');
 
+let currentGnbItems = [];
+
 hamburgerButton.addEventListener('click', openSidebarGnb);
 closeButton.addEventListener('click', closeSidebarGnb);
 
 function openSidebarGnb() {
-  if (!sidebarGnb.classList.contains('link')) {
-    sidebarGnb.classList.add('is-active');
-    document.body.style.overflowY = 'hidden';
-  }
+  sidebarGnb.classList.add('is-active');
+  document.body.style.overflowY = 'hidden';
 }
 
 function closeSidebarGnb() {
-  if (!sidebarGnb.classList.contains('link')) {
-    sidebarGnb.classList.remove('is-active');
-    document.body.style.overflowY = 'auto';
-  }
+  sidebarGnb.classList.remove('is-active');
+  document.body.style.overflowY = 'auto';
+
+  sidebarGnbItems.forEach((item) => {
+    const gnbLink = item.querySelector('.gnb-link');
+    item.classList.remove('is-active');
+    gnbLink.ariaExpanded = false;
+  });
 }
 
 sidebarGnbItems.forEach((item) => {
-  item.addEventListener('click', () => {
-    item.classList.toggle('is-active');
-  });
+  item.addEventListener('click', () => toggleSidebarGnbItem(item));
 });
+
+function toggleSidebarGnbItem(item) {
+  const gnbLink = item.querySelector('.gnb-link');
+
+  if (!currentGnbItems.includes(item)) {
+    currentGnbItems = [...currentGnbItems, item];
+    item.classList.add('is-active');
+
+    gnbLink.ariaExpanded = 'true';
+  } else if (currentGnbItems.includes(item)) {
+    currentGnbItems = currentGnbItems.filter((currentItem) => {
+      return currentItem !== item;
+    });
+    item.classList.remove('is-active');
+    gnbLink.ariaExpanded = 'false';
+  }
+}
